f7cfa1f7565ab100ed738a98479f19572feee403
Guy-Ritchie <guyritchieshithole007@gmail.com>
2022-11-19 01:40:27 +0530

    Add env variables to the dbServer app, and then re-added it to be tracked by git

---
+++ dbServer.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const mysql = require("mysql")
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
+const DB_PORT = process.env.DB_PORT
+
+const db = mysql.createPool({
+    connectionLimit: 100,
+    host: DB_HOST,          // Localhost IP
+    user: DB_USER,            // The SQL User created to connect the node app
+    password: DB_PASSWORD,  // password for the new Sequel User
+    database: DB_DATABASE,         // Name of Database we want to connect to
+    port: DB_PORT                // Port number, the default value is used.
+})
+
+db.getConnection( (err, connection) => {
+    if (err) throw (err)
+    console.log("DB connected successful: "+connection.threadId)
+})
