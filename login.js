a9195e7d3289b3dd708b55f137a829bf0e1a54f4
Guy-Ritchie <guyritchieshithole007@gmail.com>
2022-11-20 21:36:01 +0530

    Fully working website

--- login.js
+++ login.js
@@ -19,7 +19,14 @@ const connection = mysql.createConnection({
     database: DB_DATABASE
 });
 
+// Creating an express application
 const app = express();
+
+// storing the value of username for further displaying the respective user's info
+let uname = '';
+
+// setting the view engine to ejs -> as I wanted to render the user info dynamically, and have it viewed through an engine [This was the only possible way to render / pass values to HTML]
+app.set('view engine', 'ejs');
 
 app.use(session({
     secret: 'secret',
@@ -40,6 +47,7 @@ app.get('/', function(request, response) {
 app.post('/auth', function(request, response) {
     // Capture the input fields
     let username = request.body.username;
+    uname = username;
     let password = request.body.password;
     // Ensure the input fields exist and are not empty
     if (username && password) {
@@ -82,7 +90,50 @@ app.get('/home', function(request, response) {
 });
 
 // http://localhost:3000/dashboard
+app.post('/dashboard', function(request, response) {
+    // Return the dashboard for the user if he is logged in.
+    if (request.session.loggedin) {
+        // // Return the dashboard to be viewed for the user.
+        // response.sendFile(path.join(__dirname + '/dashboard.html'));
+        // Instead of sending the path and hence thereby opening the html file, we can instead render an ejs file, using the ejs view engine.
+        response.render('dashboard');
+    } else {
+        // Not logged in
+        response.send('Please login to view this page!');
+    }
+    //response.end();
+});
 
+// http://localhost:3000/info
+app.post('/info', function(request, response) {
+    // Return the dashboard for the user if he is logged in.
+    if (request.session.loggedin) {
+        // // Return the dashboard to be viewed for the user.
+        // response.sendFile(path.join(__dirname + '/info.html'));
+        // Instead of sending the file and it's path, to be displayed when accessing this route, we can actually pass the sql values as parameters to the view engine, to print them out, dynamically corresponding to the data available from the sql database.
+        // response.render('info', { rinzler: 'Hello Tron!'});
+        // console.log(uname);
+        connection.query('SELECT id,username,email FROM accounts WHERE username = ?',[uname], function(error,results,fields){
+            // Output error if there is some kind of problem with the query
+            if (error) throw error;
+            // If the account exists then perform the operations associated with it!
+            else {
+                // console.log(results);
+                // // This basically stores all the resulting row's fields in it.
+                // // In the following steps, we have to seperate the different fields (4 of them) and just show only the three fields to the user.
+                // // Also try to change the query so that only the three non-private fields are sent back as a result
+                // console.log("Id is : ",results[0].id);
+                // console.log("Username is : ",results[0].username);
+                // console.log("Email is : ",results[0].email);
+                response.render('info', { id: results[0].id, name: results[0].username, mail: results[0].email})
+            }
+        })
+    } else {
+        // Not logged in
+        response.send('Please login to view this page!');
+    }
+    //response.end();
+});
 
 // Add a port to enable the nodejs server to listen to incoming connections
 // Ideally when we want to deploy our login system to a production server, we want to listen on port `80` so that we don't have to specify the port number in the URL.
